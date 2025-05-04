00d1370ecb0fd7124ec34bd4af9379d083944e27
wildan <wildanzhaf@gmail.com>
2025-05-04 21:57:58 +0700

    types: define UpcomingEvent interface for table rendering

---
+++ src/types/upcomingEvent.types.ts
@@ -0,0 +1,11 @@
+export interface UpcomingEvent {
+  id: number;
+  image: string;
+  title: string;
+  category: string;
+  date: string;
+  time: string;
+  venue: string;
+  city: string;
+  priceRange: string;
+}
