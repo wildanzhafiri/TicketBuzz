ba061c93072da41009fa31347783d22ccbffb271
wildan <wildanzhaf@gmail.com>
2025-05-04 21:55:39 +0700

    feat: implement HeroSection with headline and call to action

---
+++ src/components/homepage/HeroSection.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import banner from '../../assets/bannerhomepage.svg';
+
+const HeroSection: React.FC = () => {
+  return (
+    <section className="w-full h-[500px] bg-cover bg-center text-white flex items-center" style={{ backgroundImage: `url(${banner})` }}>
+      <div className="w-full h-full flex items-center">
+        <div className="max-w-5xl px-36">
+          <h1 className="text-5xl md:text-7xl font-extrabold mb-6 leading-16 font-plus-jakarta-sans">
+            Find Your Next <br />
+            Concert Experience
+          </h1>
+          <p className="text-xl md:text-2xl mb-8 max-w-2xl font-plus-jakarta-sans font-medium">Discover and book tickets to the hottest concerts and live music events happening near you.</p>
+          <div className="flex gap-4 font-plus-jakarta-sans font-bold">
+            <button className="bg-[#7F27D4] hover:bg-purple-700 text-white font-semibold py-3 px-6 rounded-lg">Explore Events</button>
+            <button className="bg-white text-[#5925BB] hover:bg-gray-100 font-semibold py-3 px-6 rounded-lg">How it Works</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
