bda7fa9b9f0931360195342e0d72ef555621069e
wildan <wildanzhaf@gmail.com>
2025-05-25 22:06:20 +0700

    style: add top margin to content to offset fixed navbar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,7 +5,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="w-full mx-auto flex-grow">{children}</main>
+      <main className="w-full mx-auto mt-20 flex-grow">{children}</main>
       <Footer />
     </div>
   );
