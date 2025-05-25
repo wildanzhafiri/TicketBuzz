06ea23035043c671bde1c8642e97cff7c0f3bcb6
wildan <wildanzhaf@gmail.com>
2025-05-25 20:44:37 +0700

    feat: wrap app with AuthProvider for global authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import AppRoutes from './routes/AppRoutes';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <AppRoutes />
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
       </BrowserRouter>
     </>
   );
