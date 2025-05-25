27727e1704fa69ebfab7a20f11003e0b34dddd74
wildan <wildanzhaf@gmail.com>
2025-05-25 20:31:34 +0700

    feat: update EventItem and add Concert interface for detailed data handling

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,10 +1,29 @@
 export interface EventItem {
   id: number;
   title: string;
+  category?: string;
   location: string;
   date: string;
   time: string;
-  priceRange: string;
-  category: string;
+  priceRange?: string;
   image: string;
+  description?: string;
+  venueMap?: string;
+  venueInfo?: string;
+}
+
+export interface Concert {
+  id: number;
+  name: string;
+  concert_start: string;
+  concert_end: string;
+  link_poster: string;
+  genres: { name: string }[];
+  venue: {
+    name: string;
+    city: { name: string };
+  };
+  tickets: {
+    price: number;
+  }[];
 }
