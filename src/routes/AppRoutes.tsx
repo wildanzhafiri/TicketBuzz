2f7af555df371c1defc5df21122b46f4167acd0d
wildan <wildanzhaf@gmail.com>
2025-05-25 20:32:57 +0700

    feat: integrate new pages into AppRoutes with layout support

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Homepage from '../pages/Homepage';
+import EventDetail from '../pages/EventDetail';
+import Catalog from '../pages/Catalog';
+import ProtectedRoute from './ProtectedRoute';
+import MyTicket from '../pages/MyTicket';
+import OAuthCallback from '../components/auth/OAuthCallback';
 
 const AppRoutes = () => {
   return (
@@ -14,6 +19,32 @@ const AppRoutes = () => {
             </MainLayout>
           }
         />
+        <Route
+          path="/concert/:id"
+          element={
+            <MainLayout>
+              <EventDetail />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/catalog"
+          element={
+            <MainLayout>
+              <Catalog />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/my-ticket"
+          element={
+            <MainLayout>
+              <MyTicket />
+            </MainLayout>
+          }
+        />
+
+        <Route path="/oauth/callback" element={<OAuthCallback />} />
       </Routes>
     </>
   );
